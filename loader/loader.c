93d12c807aa40bf61f85dc5fcd83837d1891a1a4
Anemona Maria M <57135826+anemonamaria@users.noreply.github.com>
2022-04-20 21:54:16 +0300

    Add files via upload

--- loader/loader.c
+++ loader/loader.c
@@ -1,7 +1,7 @@
 /*
  * Loader Implementation
  *
- * 2018, Operating Systems
+ * 2022, Operating Systems
  */
 
 #include <stdio.h>
@@ -14,90 +14,72 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <unistd.h>
-#include <errno.h>
 
 #include "exec_parser.h"
 
 static so_exec_t *exec;
 static int fd;
 
-void so_sigaction(int sig_no, siginfo_t *sig_info, void *context)
+void so_sa_sigaction(int signum, siginfo_t *info, void *context)
 {
-	if(sig_no != SIGSEGV) 
+	if (signum != SIGSEGV) 
 		return;
 	
-	if (sig_info == NULL)
-		exit(EXIT_FAILURE);
-
-	int page_fault_addr = (int)sig_info->si_addr;
-	void *prev_handler;
+	if (info == NULL)
+		return;
 
 	for (int i = 0; i < exec->segments_no; i++) {
-		so_seg_t *segment = (exec->segments) + i;
+		so_seg_t *seg = (exec->segments) + i;
+		int s_vaddr = seg->vaddr;
 
-
-		if (segment->vaddr <= page_fault_addr &&
-			page_fault_addr < segment->vaddr + segment->mem_size) {
-			if (segment->data == NULL) {
-				int possible_pages = segment->mem_size / getpagesize();
-
-				segment->data = (void *) malloc(possible_pages * sizeof(char));
-				// memset(segment->data, 0, possible_pages * sizeof(char));
+		if (s_vaddr <= (int)info->si_addr &&
+			(int)info->si_addr < s_vaddr + seg->mem_size) {
+			if (seg->data == NULL) {
+				int page = ((int)info->si_addr + s_vaddr) / getpagesize();
+				seg->data = (void *) calloc(page, sizeof(char));
 			}
 
-			int page_index = (page_fault_addr - segment->vaddr) / getpagesize();
-			uintptr_t page_addr = page_index * getpagesize();
+			// retin adresa de start
+			int page_start = ((int)info->si_addr - s_vaddr) / getpagesize();
 
-			if (((char *)(segment->data))[page_index] == 1) {
-				fprintf(stderr, "Invalid permissions\n");
-				signal(SIGSEGV, prev_handler);
-				// raise(SIGSEGV);
+			if (((char *)(seg->data))[page_start] != 1) {
+				((char *)(seg->data))[page_start] = 1;
+			} else {
+				signal(SIGSEGV, NULL);
+				return;
+			}
+			// mapez pagina
+			char *mmap_ret = mmap((void *)(s_vaddr + page_start * getpagesize()), 
+				getpagesize(), PROT_WRITE, MAP_FIXED | MAP_ANON | MAP_PRIVATE, -1, 0);
+
+			if (mmap_ret == MAP_FAILED) {
+				return;
 			}
 
-			char *mapped_addr = mmap((void *)(segment->vaddr + page_addr), getpagesize(), PROT_WRITE,
-				MAP_SHARED | MAP_FIXED | MAP_ANON, -1, 0);
-
-			if (mapped_addr == MAP_FAILED) {
-				fprintf(stderr, "Error mapping page.\nerror numner: %d\n", errno);
-				exit(EXIT_FAILURE);
-			}
-
-			((char *)(segment->data))[page_index] = 1;
-
-			if (page_addr < segment->file_size) {
-				lseek(fd, segment->offset + page_addr, SEEK_SET);
-
+			int size = seg->file_size - page_start * getpagesize();
+			if (size > 0) {
+				int ret = lseek(fd, seg->offset + page_start * getpagesize(),
+						SEEK_SET);
+				if (ret < 0)
+					return;
 				
-				int bytes_read;
-				if((segment->file_size - page_addr) < getpagesize()) 
-					bytes_read = read(fd, (void *) mapped_addr, segment->file_size - page_addr);
+				int read_ret;
+				if(size - getpagesize() < 0)  // citesc in memorie
+					read_ret = read(fd, (void *) mmap_ret, size);
 				else 
-					bytes_read = read(fd, (void *) mapped_addr, getpagesize());
+					read_ret = read(fd, (void *) mmap_ret, getpagesize());
 
-				if (bytes_read == -1) {
-					fprintf(stderr, "Error reading from file\n");
-					exit(EXIT_FAILURE);
+				if (read_ret == -1) {
+					return;
 				}
 			}
 
-			mprotect(mapped_addr, getpagesize(), PROT_NONE);
-
-			int perm_flags = 0;
-
-			if ((segment->perm & PERM_R) != 0)
-				perm_flags |= PROT_READ;
-			if ((segment->perm & PERM_W) != 0)
-				perm_flags |= PROT_WRITE;
-			if ((segment->perm & PERM_X) != 0)
-				perm_flags |= PROT_EXEC;
-
-			mprotect(mapped_addr, getpagesize(), perm_flags);
+			mprotect(mmap_ret, getpagesize(), seg->perm);
 			return;
 		}
 	}
 
-	fprintf(stderr, "Seg Fault outside of Exec segments\n");
-	signal(SIGSEGV, prev_handler);
+	signal(SIGSEGV, NULL);
 }
 
 int so_init_loader(void)
@@ -105,7 +87,7 @@ int so_init_loader(void)
 	struct sigaction sa;
 
 	memset(&sa, 0, sizeof(struct sigaction));
-	sa.sa_sigaction = so_sigaction;
+	sa.sa_sigaction = so_sa_sigaction;
 	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
 	sigaddset(&sa.sa_mask, SIGSEGV);
