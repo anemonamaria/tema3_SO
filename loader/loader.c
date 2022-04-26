2730e8ae3fcec72ac487c2ba0c8fba30450970ca
Anemona Maria M <57135826+anemonamaria@users.noreply.github.com>
2022-04-26 11:53:21 +0300

    Add files via upload

--- loader/loader.c
+++ loader/loader.c
@@ -18,41 +18,45 @@
 #include "exec_parser.h"
 
 static so_exec_t *exec;
+static struct sigaction old_action;
 static int fd;
 
 void so_sa_sigaction(int signum, siginfo_t *info, void *context)
 {
-	if (signum != SIGSEGV) 
+	if (signum != SIGSEGV) {
+		old_action.sa_sigaction(signum, info, context);
 		return;
-	
-	if (info == NULL)
+	}
+
+	if (info == NULL) {
+		old_action.sa_sigaction(signum, info, context);
 		return;
+	}
 
 	for (int i = 0; i < exec->segments_no; i++) {
+		// parcurg vectorul de segmente
 		so_seg_t *seg = (exec->segments) + i;
 		int s_vaddr = seg->vaddr;
+		
+		if (seg->data == NULL) {
+			int page = ((int)info->si_addr + s_vaddr) / getpagesize();
+
+			seg->data = (void *) calloc(page, sizeof(char));
+		}
+
+		// retin adresa de start		
+		int page_start = ((int)info->si_addr - s_vaddr) / getpagesize();
 
 		if (s_vaddr <= (int)info->si_addr &&
-			(int)info->si_addr < s_vaddr + seg->mem_size) {
-			if (seg->data == NULL) {
-				int page = ((int)info->si_addr + s_vaddr) / getpagesize();
-				seg->data = (void *) calloc(page, sizeof(char));
-			}
-
-			// retin adresa de start
-			int page_start = ((int)info->si_addr - s_vaddr) / getpagesize();
-
-			if (((char *)(seg->data))[page_start] != 1) {
-				((char *)(seg->data))[page_start] = 1;
-			} else {
-				signal(SIGSEGV, NULL);
-				return;
-			}
-			// mapez pagina
-			char *mmap_ret = mmap((void *)(s_vaddr + page_start * getpagesize()), 
-				getpagesize(), PROT_WRITE, MAP_FIXED | MAP_ANON | MAP_PRIVATE, -1, 0);
+			(int)info->si_addr < s_vaddr + seg->mem_size
+			&& ((char *)(seg->data))[page_start] == 0) {
+						
+			char *mmap_ret = mmap((s_vaddr + page_start * getpagesize()), 
+				getpagesize(), PROT_WRITE, MAP_FIXED | MAP_ANONYMOUS | 
+				MAP_PRIVATE, -1, 0);
 
 			if (mmap_ret == MAP_FAILED) {
+				old_action.sa_sigaction(signum, info, context);
 				return;
 			}
 
@@ -60,23 +64,29 @@ void so_sa_sigaction(int signum, siginfo_t *info, void *context)
 			if (size > 0) {
 				int ret = lseek(fd, seg->offset + page_start * getpagesize(),
 						SEEK_SET);
-				if (ret < 0)
+				if (ret < 0) {
+					old_action.sa_sigaction(signum, info, context);
 					return;
-				
+				}
+
 				int read_ret;
-				if(size - getpagesize() < 0)  // citesc in memorie
+
+				if (size - getpagesize() < 0)  // citesc in memorie
 					read_ret = read(fd, (void *) mmap_ret, size);
 				else 
 					read_ret = read(fd, (void *) mmap_ret, getpagesize());
 
-				if (read_ret == -1) {
+				if (read_ret == -1)  {
+					old_action.sa_sigaction(signum, info, context);
 					return;
 				}
 			}
+			
+			((char *)(seg->data))[page_start] = size;
 
 			mprotect(mmap_ret, getpagesize(), seg->perm);
 			return;
-		}
+		} 
 	}
 
 	signal(SIGSEGV, NULL);
@@ -92,7 +102,7 @@ int so_init_loader(void)
 	sigemptyset(&sa.sa_mask);
 	sigaddset(&sa.sa_mask, SIGSEGV);
 	
-	int ret = sigaction(SIGSEGV, &sa, NULL);
+	int ret = sigaction(SIGSEGV, &sa, &old_action);
 	if (ret < 0)
 		return -1;
 
